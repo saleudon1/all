78969cc5d360adf7235e75ce87ea56fb32dcde69
saleudon1 <saleudon1@gmail.com>
2025-11-20 22:08:11 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || "https://sogqxt.tempavatar.click",
+  origin: process.env.CORS_ORIGIN || "https://future.tskg.dpdns.org",
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
